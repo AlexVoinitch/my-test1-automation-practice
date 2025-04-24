75645a408855f245279778eb967028f24337c6e6
Alexei Voinitch <alexei.voinitch@gmail.com>
2025-04-24 17:32:05 +0200

    Creating new files and add context there.

--- cypress/support/requirements.js
+++ cypress/support/requirements.js
@@ -1,3 +1,4 @@
-export const requirements = {
-  minPasswordLength: 8
-};
+// cypress/support/requirements.js
+export const standardUsername = 'standard_user';
+export const validPassword = 'secret_sauce';
+// Добавь другие требования по мере необходимости
