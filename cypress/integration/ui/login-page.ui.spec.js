21459326e5f27538e6164947871817f740aeb198
Alexei Voinitch <alexei.voinitch@gmail.com>
2025-04-24 18:53:30 +0200

    Creating new files and add context there.

--- cypress/integration/ui/login-page.ui.spec.js
+++ cypress/integration/ui/login-page.ui.spec.js
@@ -11,7 +11,11 @@ describe('Login Page UI', { testIsolation: false }, () => {
     });
 
     it('When I enter a valid username', () => {
-      cy.get(cy.selectors.loginPage.usernameInput).type(cy.reqs.standardUsername);
+      // Добавлено ожидание, что элемент будет видим
+      cy.get(cy.selectors.loginPage.usernameInput, { timeout: 10000 }).should('be.visible').type(cy.reqs.standardUsername);
+
+      // Добавлено использование debug для отладки элемента
+      cy.get(cy.selectors.loginPage.usernameInput).debug();
     });
 
     it('And I enter a valid password', () => {
@@ -31,7 +35,6 @@ describe('Login Page UI', { testIsolation: false }, () => {
     });
   });
 
-
   context('As a user, I want to see an error message when I log in with invalid credentials', () => {
     it.skip('Given I am on the login page', () => {
       // Not implemented yet
@@ -45,66 +48,4 @@ describe('Login Page UI', { testIsolation: false }, () => {
       // Not implemented yet
     });
 
-    it.skip('Then I should see an error message indicating incorrect credentials', () => {
-      // Not implemented yet
-    });
-  });
-
-  context('As a user, I want to see an error message when I log in with a valid username and an invalid password', () => {
-    it.skip('Given I am on the login page', () => {
-      // Not implemented yet
-    });
-
-    it.skip('When I enter a valid username and an invalid password', () => {
-      // Not implemented yet
-    });
-
-    it.skip('And I click the "Login" button', () => {
-      // Not implemented yet
-    });
-
-    it.skip('Then I should see an error message indicating incorrect credentials', () => {
-      // Not implemented yet
-    });
-  });
-
-  context('As a locked-out user, I want to see a specific error message', () => {
-    it.skip('Given I am on the login page', () => {
-      // Not implemented yet
-    });
-
-    it.skip('When I enter the username of a locked-out user and any password', () => {
-      // Not implemented yet
-    });
-
-    it.skip('And I click the "Login" button', () => {
-      // Not implemented yet
-    });
-
-    it.skip('Then I should see an error message indicating that the user is locked out', () => {
-      // Not implemented yet
-    });
-  });
-
-  context('As a user, I want to see an error message when I try to log in with empty fields', () => {
-    it.skip('Given I am on the login page', () => {
-      // Not implemented yet
-    });
-
-    it.skip('When I leave the username and password fields empty', () => {
-      // Not implemented yet
-    });
-
-    it.skip('And I click the "Login" button', () => {
-      // Not implemented yet
-    });
-
-    it.skip('Then I should see an error message indicating that the username is required', () => {
-      // Not implemented yet
-    });
-
-    it.skip('And I should see an error message indicating that the password is required', () => {
-      // Not implemented yet
-    });
-  });
-});
+    it.skip('Then I should see an error message indicating
