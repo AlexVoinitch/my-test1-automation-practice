6337e1da38270c727282f4fd5f0a3f10e427ec58
Alexei Voinitch <alexei.voinitch@gmail.com>
2025-05-26 22:28:03 +0200

    Refactor login page UI tests based on review

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,12 +1,14 @@
 // cypress/support/e2e.js
+
 import * as l10n from './l10n.json';
 import * as reqs from './requirements.js';
 import * as urls from './urls.js';
 import * as selectors from './selectors.js';
 
+cy.urls = urls;
+cy.selectors = selectors;
+
 Cypress.on('test:before:run', () => {
-  cy.l10n = l10n;
   cy.reqs = reqs;
-  cy.urls = urls;
-  cy.selectors = selectors;
+  cy.l10n = l10n;
 });
